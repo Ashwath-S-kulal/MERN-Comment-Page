dd17bb27c7e729cf61724f82312c06370273472b
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2025-08-13 21:16:01 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors()); 
 
+
+
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/project_comments';
