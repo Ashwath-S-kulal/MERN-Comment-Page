1cd38fe9fba93aad6c9e5639e4f15cfc3e470e8a
Ashwath-S-kulal <ashwathkulal2004@gmail.com>
2025-08-13 21:07:17 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Project from './pages/Project';
 import Home from "./pages/Home";
 import ReplyForm from './pages/ReplayForm';
 
+
+
+
 export default function App() {
   return (
     <div className='bg-gray-900 min-h-screen min-w-fit text-white'>
